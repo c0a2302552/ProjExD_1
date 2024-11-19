0ee164c16bac4bef527d54ba12c1600873a30c75
Haruto Ooka <c0a2302552@edu.teu.ac.jp>
2024-11-19 17:03:00 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -10,45 +10,40 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2=pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
-    kk_rct = kk_img.get_rect()  # こうかとんRectを取得する
+    kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
- 
-   
     tmr = 0
-
-    bg_x=0
-
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
-        # print(key_lst[pg.K_UP], key_lst[pg.K_DOWN], 
-        #       key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
-        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
-            kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
-            kk_rct.move_ip(+1, 0)
+        dx, dy = 0,0
+        #print(key_lst[pg.K_UP], key_lst[pg.K_DOWN],
+        #key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
+        if key_lst[pg.K_UP]:
+            dy -= 1
+            dx -= 1
+        if key_lst[pg.K_DOWN]:
+            dy += 1
+            dx -= 1
+        if key_lst[pg.K_LEFT]:
+            dx -= 1
+        if key_lst[pg.K_RIGHT]:
+            dx += 1
+        if dx == 0 and dy == 0:
+            dx = -1
+        kk_rct.move_ip(dx, dy)
 
-
-
-        x=-(tmr%3200)
-        screen.blit(bg_img,[x,0])
-        screen.blit(bg_img2,[x+1600,0]) 
-        screen.blit(bg_img,[x+3200,0])
-        screen.blit(bg_img2,[x+4800,0])
-
+        x = -(tmr%3200)
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x + 1600, 0])
+        screen.blit(bg_img, [x + 3200, 0])
+        screen.blit(bg_img2, [x + 4800, 0])
         screen.blit(kk_img, kk_rct)
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)
@@ -58,4 +53,4 @@ if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
-    sys.exit() #
+    sys.exit()
