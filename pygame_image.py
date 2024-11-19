0e6b52db8915440b9f52a0bf93e91857e2ddd204
Haruto Ooka <c0a2302552@edu.teu.ac.jp>
2024-11-19 16:25:41 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -27,14 +27,11 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-
-        
-
-        screen.blit(bg_img, [-(tmr%1600), 0])#screan sarfaceに貼り付け
-        screen.blit(bg_img2, [-((tmr+1600)%1600), 0])
-        screen.blit(bg_img, [-(tmr%1600), 0])
-        screen.blit(bg_img2, [-((tmr+1600)%1600), 0])
+        x=-(tmr%3200)
+        screen.blit(bg_img,[x,0])
+        screen.blit(bg_img2,[x+1600,0]) 
+        screen.blit(bg_img,[x+3200,0])
+        screen.blit(bg_img2,[x+4800,0])
         screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
